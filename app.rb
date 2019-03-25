aea29761ed769884ca96f1b1e16562410afcc7d3
sonota88 <yosiot8753@gmail.com>
2019-03-26 07:37:55 +0900

    Server side queue

--- app.rb
+++ app.rb
@@ -1,11 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+$q = Thread::Queue.new
+
+# dummy enqueue
+Thread.new do
+  loop do
+    sleep 5
+    $q.enq Time.now.to_s
+  end
+end
+
 get "/" do
   send_file "index.html"
 end
 
 post "/comet/open" do
-  sleep 1
-  "TODO #{Time.now}"
+  msg = $q.deq
+  msg
 end
