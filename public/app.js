b160884ea3ee3afa8ebc5172dfe28230073ceb74
sonota88 <yosiot8753@gmail.com>
2019-03-26 07:18:45 +0900

    class Comet and Page

--- public/app.js
+++ public/app.js
@@ -1,3 +1,21 @@
 const puts = console.log.bind(console);
+const debug = console.debug.bind(console);
 
-puts("hi");
+class Comet {
+
+}
+
+class Page {
+  constructor(){
+    this.comet = new Comet();
+  }
+
+  start(){
+    debug("-->> start");
+  }
+}
+
+$(()=>{
+  window.__p = new Page();
+  __p.start();
+});
